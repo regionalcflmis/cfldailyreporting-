23be53e2a90efcf4b6cae6ad38c4a81d1f83d230
Vivek Mishra <regionalcflmis@gmail.com>
2026-09-13 23:28:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CFL_PWA_CONFIG = {
-  appUrl: 'https://script.google.com/macros/s/AKfycbyPOALBN1zcYyMS6FjFxBKq4jjz7gUNDklgG635MXXMsQwKKE6aKt3-Vn5A3IVoKgei/exec',
+  appUrl: 'https://script.google.com/macros/s/AKfycbyeTTmWOz7Rb0QoEKQZegIexx0JknfD7Oo9J3U4C9Hp3zZZ6EnzqqC6eHwHNBWGYfsZ/exec',
   startPage: 'mobile',
   appName: 'CFL Daily Data Input',
   loadTimeoutMs: 30000
